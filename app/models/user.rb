4940b3ac6ae99ced6960c2b3994dbbedd17235ce
Giovanny Salcedo <giovanny.salcedo@symplifica.com>
2023-02-21 16:16:09 -0500

    model listos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_presence_of :username, :fullname, :phone
+  validates_uniqueness_of :username, :fullname, :phone
+  has_many :posts
+  has_many :comments
 end
